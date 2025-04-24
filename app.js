c83e595a25796d81d571932cf81749e6253bbd88
inshah20 <inshahjamal26@gmail.com>
2025-04-24 06:39:09 +0500

    completed assignment 43-48

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 
 // Qno:2
-function purchasing() {
-    alert("Thanks For Purchasing a Phone From us!")
-}
+// function purchasing() {
+//     alert("Thanks For Purchasing a Phone From us!")
+// }
 
 // Qno:3
 // function remove(e){
 //     e.parentNode.parentNode.remove()
 // }
+
+// Qno:5
+
+// function forward(){
+   
+//     let number = document.getElementById("counter").innerHTML
+//     number =parseInt(number)
+//     console.log(number)
+//     number = number + 1
+//     document.getElementById("counter").innerHTML = number
+// }
+
+// function backward() {
+
+//     let number = document.getElementById("counter").innerHTML
+//     number = parseInt(number)
+//     number = number -1
+//     document.getElementById("counter").innerHTML = number
+// }
+
+
+
+
+
+
+
+
